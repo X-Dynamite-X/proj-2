18dc1ade889522f2606bf4a39502ef2906462741
Dynamite <abdmoklss@gmail.com>
2024-01-14 19:15:20 +0300

    win_pkg

---
+++ install_pkg_for_win.bat
@@ -0,0 +1,11 @@
+pip install django
+pip install --upgrade pip
+pip install phonenumbers
+pip install django_phonenumbers
+pip install django-phonenumber-field
+pip install emoji
+pip install morse3
+pip install pandas
+pip install moviepy
+
+pip install --upgrade pip
